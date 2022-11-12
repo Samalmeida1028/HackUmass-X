3dd2a6f932d325079e53e0079d190a7922905c01
Sam Almeida <80793329+Samalmeida1028@users.noreply.github.com>
2022-11-11 23:35:53 -0500

    added fastled stuff

--- pictionary/pictionary.ino
+++ pictionary/pictionary.ino
@@ -5,10 +5,12 @@ November 11-13, 2022
 */
 
 // Global Variables
+#include "FastLED.h"
 
+CRGB leds[1];
 #define NUM_LEDS 256
-#define DATA_PIN 6
-CRGB leds[NUM_LEDS];
+#define DATA_PIN D1
+int ledsa[NUM_LEDS];
 const int larray[32][8]= {
   {0,1,2,3,4,5,6,7},{15,14,13,12,11,10,9,8},{16,17,18,19,20,21,22,23},{31,30,29,28,27,26,25,24},
   {32,33,34,35,36,37,38,39},{47,46,45,44,43,42,41,40},{48,49,50,51,52,53,54,55},{63,62,61,60,59,58,57,56},
@@ -28,6 +30,8 @@ const int larray[32][8]= {
 char request[] = "link";
 
 void setup() {
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS); 
+  FastLED.setBrightness(10);
   // put your setup code here, to run once:
 
   
