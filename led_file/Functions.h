c99508208c7b8a4e361e3f5fc7a29b53a4761474
Sam Almeida <80793329+Samalmeida1028@users.noreply.github.com>
2022-11-12 16:19:57 -0500

    some small changes

---
+++ led_file/Functions.h
@@ -0,0 +1,11 @@
+
+
+String httpGETRequest(const char* serverName);
+
+void httpPOST(const char* serverName, String payload);
+
+int convertToLED (int x, int y);
+
+String GetPixelsLit();
+
+String GetPixelVal(int x, int y);
--- led_file/led_file.ino
+++ led_file/led_file.ino
@@ -2,18 +2,20 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 #include <Arduino_JSON.h>
+#include "Functions.h"
 #include "FastLED.h"
 #define FASTLED_ESP8266_D1_PIN_ORDER
 #define NUM_LEDS 512
 #define DATA_PIN 5
-const char* ssid = "NoSignal";
+const char* ssid = "NoSignal";  
 const char* password = "calebpatsam";
 const char* serverName = "http://68.183.25.122:3000/test";
 
 CRGB leds[NUM_LEDS];
 int speedd = 500;
   
-  void setup() { 
+  
+void setup() { 
     Serial.begin(9600);
     FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS); 
   FastLED.setBrightness(10);
@@ -33,21 +35,7 @@ int speedd = 500;
   
   
   void loop() {
-      String test = httpGETRequest(serverName);
-       delay(500);
-       Serial.println(test);
-      for(int i = 0; i < 32; i++){
-        for(int j = 0; j < 16; j++){
-          leds[convertToLED(i, j)] = CRGB::White; 
-          FastLED.show(); 
-          delay(speedd);
-          String lit = GetPixelsLit();
-          httpPOST("http://68.183.25.122:3000/matrix", lit);
-          Serial.println(lit);
-          leds[convertToLED(i, j)] = CRGB::Black;
-          FastLED.show(); 
-        }
-      }
+    test();
   }
 
   String GetPixelVal(int x, int y){
@@ -55,12 +43,112 @@ int speedd = 500;
     return String(HexRGB, HEX);
   }
 
-//{{hex:"HEXNUMBER", coord: {X:x,Y:y}},...,n}
-//
-//
 
 
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+
+  http.begin(client, serverName);
 
 
+  int httpResponseCode = http.GET();
+  
+
+  String payload = "{}"; 
+
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
+
+
+void httpPOST(const char* serverName, String payload) {
+  WiFiClient client;
+  HTTPClient http;
+   
+
+  // Your IP address with path or Domain name with URL path 
+  http.begin(client, serverName);
+  http.addHeader("Content-Type", "text/plain");
+
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(payload);
+  
+
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();
+
+}
+
+ int convertToLED (int x, int y){
+    int temp = 8*x;
+    if(y > 7){
+      temp += 256;
+      y%=8;
+    }
+    if(x%2 == 0){
+      temp = temp +  y;
+    }
+    else{
+      temp = temp + (7-y);
+    }
+
+    return temp;
+  }
+
+    String GetPixelsLit(){
+        String jsonString = "[";
+        
+        for(int i = 0; i < 32; i++){
+        for(int j = 0; j < 16; j++){
+          String val = GetPixelVal(i,j);
+          if(val!="0"){
+          String temp = "{hex:" + val + ",coord:{x:" + String(i,DEC) + ",y:" + String(j,DEC) + "}},";
+          jsonString += temp;
+          }
+        }
+        }
+        int len = jsonString.length()-1;
+        jsonString.remove(len);
+        jsonString = jsonString + "]";
+        return jsonString;
+  }
+
+void test()
+{
+
+      String test = httpGETRequest(serverName);
+      delay(500);
+      Serial.println(test);
+      for(int i = 0; i < 32; i++){
+        for(int j = 0; j < 16; j++){
+          leds[convertToLED(i, j)] = CRGB::White; 
+          FastLED.show(); 
+          delay(speedd);
+          String lit = GetPixelsLit();
+          httpPOST("http://68.183.25.122:3000/matrix", lit);
+          //Serial.println(lit);
+          //leds[convertToLED(i, j)] = CRGB::Black;
+          //FastLED.show(); 
+        }
+      }
+}
 
   
