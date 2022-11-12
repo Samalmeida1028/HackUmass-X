62ff4e739f7bd9fd187dac32a765d367ee1c7cde
Sam Almeida <80793329+Samalmeida1028@users.noreply.github.com>
2022-11-12 16:50:44 -0500

    added some documentation

--- led_file/Functions.h
+++ led_file/Functions.h
@@ -1,11 +1,36 @@
 
 
+/*
+ *Function: httpGETRequest
+ * http request from server, takes in the serverName
+ */
 String httpGETRequest(const char* serverName);
 
+
+/*
+ *Function: httpPOST
+ * used to update or send data to the server, has body payload type is plaintext
+ */
 void httpPOST(const char* serverName, String payload);
 
+
+/*
+ *Function: convertToLED
+ * takes in an x and y value of an array and converts it to a string value for the LED matrix
+ */
 int convertToLED (int x, int y);
 
+
+/*
+ *Function: GetPixelsLit
+ * returns the location of all pixels that are not black, and their color value
+ */
 String GetPixelsLit();
 
+/*
+ *Function: GetPixelVal
+ * returns the hex value of a pixel (RGB)
+ */
 String GetPixelVal(int x, int y);
+
+void test();
