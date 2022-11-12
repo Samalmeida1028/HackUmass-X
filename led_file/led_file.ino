526f063ce3bd7504939890bad1a23b8c84ccd90c
Sam Almeida <80793329+Samalmeida1028@users.noreply.github.com>
2022-11-12 17:11:45 -0500

    Update led_file.ino

--- led_file/led_file.ino
+++ led_file/led_file.ino
@@ -7,62 +7,62 @@
 #define FASTLED_ESP8266_D1_PIN_ORDER
 #define NUM_LEDS 512
 #define DATA_PIN 5
-const char *ssid = "NoSignal";
-const char *password = "calebpatsam";
-const char *serverName = "http://68.183.25.122:3000/test";
+const char* ssid = "NoSignal";  
+const char* password = "calebpatsam";
+const char* serverName = "http://68.183.25.122:3000/test";
 
 CRGB leds[NUM_LEDS];
 int speedd = 500;
-
-void setup()
-{
-  Serial.begin(9600);
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  
+  
+void setup() { 
+    Serial.begin(9600);
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS); 
   FastLED.setBrightness(10);
-  WiFi.begin(ssid, password);
+    WiFi.begin(ssid, password);
   Serial.println("Connecting");
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while(WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
-
+ 
   Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
-}
+  }
 
-void loop()
-{
-  test();
-}
+  
+  
+  void loop() {
+    test();
+  }
 
-String GetPixelVal(int x, int y)
-{
-  long HexRGB = ((long)leds[convertToLED(x, y)].r << 16) | ((long)leds[convertToLED(x, y)].g << 8) | (long)leds[convertToLED(x, y)].b; // get value and convert.
-  return String(HexRGB, HEX);
-}
+  String GetPixelVal(int x, int y){
+    long HexRGB = ((long)leds[convertToLED(x,y)].r << 16) | ((long)leds[convertToLED(x,y)].g << 8 ) | (long)leds[convertToLED(x,y)].b; // get value and convert.
+    return String(HexRGB, HEX);
+  }
 
-String httpGETRequest(const char *serverName)
-{
+
+
+String httpGETRequest(const char* serverName) {
   WiFiClient client;
   HTTPClient http;
 
   http.begin(client, serverName);
 
+
   int httpResponseCode = http.GET();
+  
 
-  String payload = "{}";
+  String payload = "{}"; 
 
-  if (httpResponseCode > 0)
-  {
+  if (httpResponseCode>0) {
     Serial.print("HTTP Response code: ");
     Serial.println(httpResponseCode);
     payload = http.getString();
   }
-  else
-  {
+  else {
     Serial.print("Error code: ");
     Serial.println(httpResponseCode);
   }
@@ -72,90 +72,83 @@ String httpGETRequest(const char *serverName)
   return payload;
 }
 
-void httpPOST(const char *serverName, String payload)
-{
+
+void httpPOST(const char* serverName, String payload) {
   WiFiClient client;
   HTTPClient http;
+   
 
-  // Your IP address with path or Domain name with URL path
+  // Your IP address with path or Domain name with URL path 
   http.begin(client, serverName);
-  http.addHeader("Content-Type", "text/plain");
+  http.addHeader("Content-Type", "application/json");
 
   // Send HTTP POST request
   int httpResponseCode = http.POST(payload);
+  
 
-  if (httpResponseCode > 0)
-  {
+  if (httpResponseCode>0) {
     Serial.print("HTTP Response code: ");
   }
-  else
-  {
+  else {
     Serial.print("Error code: ");
     Serial.println(httpResponseCode);
   }
   http.end();
+
 }
 
-int convertToLED(int x, int y)
-{
-  int temp = 8 * x;
-  if (y > 7)
-  {
-    temp += 256;
-    y %= 8;
-  }
-  if (x % 2 == 0)
-  {
-    temp = temp + y;
-  }
-  else
-  {
-    temp = temp + (7 - y);
-  }
-
-  return temp;
-}
-
-String GetPixelsLit()
-{
-  String jsonString = "[";
-
-  for (int i = 0; i < 32; i++)
-  {
-    for (int j = 0; j < 16; j++)
-    {
-      String val = GetPixelVal(i, j);
-      if (val != "0")
-      {
-        String temp = "{hex:" + val + ",coord:{x:" + String(i, DEC) + ",y:" + String(j, DEC) + "}},";
-        jsonString += temp;
-      }
+ int convertToLED (int x, int y){
+    int temp = 8*x;
+    if(y > 7){
+      temp += 256;
+      y%=8;
     }
+    if(x%2 == 0){
+      temp = temp +  y;
+    }
+    else{
+      temp = temp + (7-y);
+    }
+
+    return temp;
   }
-  int len = jsonString.length() - 1;
-  jsonString.remove(len);
-  jsonString = jsonString + "]";
-  return jsonString;
-}
+
+    String GetPixelsLit(){
+        String jsonString = "{pixels:[";
+        
+        for(int i = 0; i < 32; i++){
+        for(int j = 0; j < 16; j++){
+          String val = GetPixelVal(i,j);
+          if(val!="0"){
+          String temp = "{hex:" + val + ",coord:{x:" + String(i,DEC) + ",y:" + String(j,DEC) + "}},";
+          jsonString += temp;
+          }
+        }
+        }
+        int len = jsonString.length()-1;
+        jsonString.remove(len);
+        jsonString = jsonString + "]}";
+        return jsonString;
+  }
 
 void test()
 {
 
-  String test = httpGETRequest(serverName);
-  delay(500);
-  Serial.println(test);
-  for (int i = 0; i < 32; i++)
-  {
-    for (int j = 0; j < 16; j++)
-    {
-      leds[convertToLED(i, j)] = CRGB::White;
-      FastLED.show();
-      delay(speedd);
-      String lit = GetPixelsLit();
-      httpPOST("http://68.183.25.122:3000/matrix", lit);
-      // Serial.println(lit);
-      // leds[convertToLED(i, j)] = CRGB::Black;
-      // FastLED.show();
-    }
-  }
+      String test = httpGETRequest(serverName);
+      delay(500);
+      Serial.println(test);
+      for(int i = 0; i < 32; i++){
+        for(int j = 0; j < 16; j++){
+          leds[convertToLED(i, j)] = CRGB::White; 
+          FastLED.show(); 
+          delay(speedd);
+          String lit = GetPixelsLit();
+          httpPOST("http://68.183.25.122:3000/matrix", lit);
+          //Serial.println(lit);
+          //leds[convertToLED(i, j)] = CRGB::Black;
+          //FastLED.show(); 
+        }
+      }
 }
+
+  
