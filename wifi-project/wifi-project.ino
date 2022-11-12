7bf7321560fc5fc2ad77dea1760131405ba3eeb8
Sam Almeida <80793329+Samalmeida1028@users.noreply.github.com>
2022-11-12 11:59:35 -0500

    added code to connect to hotspot

---
+++ wifi-project/wifi-project.ino
@@ -0,0 +1,28 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <Arduino_JSON.h>
+
+const char* ssid = "NoSignal";
+const char* password = "calebpatsam";
+
+void setup() {
+  Serial.begin(9600); 
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+ 
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
