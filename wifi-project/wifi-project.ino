e0cbdc80caa9b53f8fb1ab979d363f20f102696e
Sam Almeida <80793329+Samalmeida1028@users.noreply.github.com>
2022-11-12 12:39:04 -0500

    Update wifi-project.ino

--- wifi-project/wifi-project.ino
+++ wifi-project/wifi-project.ino
@@ -5,6 +5,8 @@
 
 const char* ssid = "NoSignal";
 const char* password = "calebpatsam";
+const char* serverName = "http://68.183.25.122:3000/test";
+
 
 void setup() {
   Serial.begin(9600); 
@@ -23,6 +25,65 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  String test = httpGETRequest(serverName);
+  delay(500);
+  Serial.println(test);
+  httpPOST("http://68.183.25.122:3000/matrix", test);
+  delay(500);
+}
+
+
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+
+  // Your IP address with path or Domain name with URL path 
+  http.begin(client, serverName);
+
+  // If you need Node-RED/server authentication, insert user and password below
+  //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+
+  String payload = "{}"; 
+
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
+
+void httpPOST(const char* serverName, String payload) {
+  WiFiClient client;
+  HTTPClient http;
+
+  // Your IP address with path or Domain name with URL path 
+  http.begin(client, serverName);
+
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(payload);
+  
+
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();
 
 }
